e08cdcf5325069d77646f97fcae7c5559e157aee
wuwushrek <frmbouwe@gmail.com>
2018-02-25 01:58:58 +0100

    refactor for FreeRTOS + tasks identification and parameters

--- external/Aigle/include/aigle.h
+++ external/Aigle/include/aigle.h
@@ -1,6 +1,10 @@
 #ifndef AIGLE_H
 #define AIGLE_H
 
+#include "FreeRTOS.h"
+#include "task.h"
+#include <iostream>
+
 #ifdef AIGLE_SITL_MODE
 	#include "io_aigle_sitl.h"
 #else
@@ -8,19 +12,77 @@
 #endif
 
 #include "strategy.h"
-#include <iostream>
 
-
-IoAigleInterface aigle_io;
-Strategy strategy;
-
-#ifndef AIGLE_SITL_MODE
-uint8_t init_aigle();
+/* Deadlines are in millisecond */
+#ifdef AIGLE_SITL_MODE
+	#define SENSORS_DEADLINE 10UL
+	#define IMU_DEADLINE 10UL
+	#define GPS_DEADLINE 100UL
 #else
-uint8_t init_aigle(int port_to_gazebo);
+	#define IMU_DEADLINE 8UL
+	#define GPS_DEADLINE 100UL
 #endif
+/********************************/
 
-void execute_strategy();
+/* Define Task priorities									*/
+#define SENSOR_READ_PRIORITY 	( configMAX_PRIORITIES - 1 )
+#define STRATEGY_PRIORITY 		( configMAX_PRIORITIES - 2 )
+#define ESTIMATOR_PRIORITY		( configMAX_PRIORITIES - 3 )		
+/************************************************************/
 
+/* Define Task stack size (Need (a lot of) improvements) 				*/
+#ifdef AIGLE_SITL_MODE
+	#define STACK_IMU_SIZE			100
+	#define STACK_GPS_SIZE			100
+	#define STACK_STRAT_SIZE		1000
+	#define STACK_ESTIM_SIZE		1000
+#else
+	#define STACK_IMU_SIZE			50
+	#define STACK_GPS_SIZE			50
+	#define STACK_STRAT_SIZE		1000
+	#define STACK_ESTIM_SIZE		1000
+#endif
+/************************************************************/
+
+/*
+* Task main function for reading IMU data 
+* This is a periodic task with the period defined by IMU_DEADLINE
+* This task has the high priority within all the tasks
+*/
+void task_imu_read(void*  aigle_instance);
+
+/*
+* Task main function for reading GPS data 
+* This is a periodic task with the period defined by GPS_DEADLINE
+* This task has the high priority within all the tasks
+*/
+void task_gps_read(void*  aigle_instance);
+
+/*
+* Task main function for deciding when to transfer PX4 motor value or not
+* This is a non periodic task that apply the strategy then the control of
+* the motors. **TODO : Separate actuator output and Strategy using a task**
+* This task has the second high priority
+*/
+void task_strategy_control(void* strategy);
+
+/*
+* Task main function for EKF estimation of position/velocity and heading 
+* This is a non periodic task than can be preempted by all others tasks
+* SO This task has the lowest priority
+*/
+void task_ekf_estimator(void* estimator);
+
+
+#ifdef AIGLE_SITL_MODE
+/*
+* Task main function for reading IMU,GPS and motors data 
+* This is a periodic task with the period defined by SENSORS_DEADLINE
+* This task has the high priority within all the tasks
+* This is only use in a simulation context because all sensors data
+* come from the same socket so we need to dispatch them to simulate real sensors
+*/
+void task_collectSensorsData(void* aigle_instance);
+#endif
 
 #endif //AIGLE_H
--- external/Aigle/src/aigle.cpp
+++ external/Aigle/src/aigle.cpp
@@ -1,43 +1,125 @@
-
 #include "aigle.h"
 
-#ifdef AIGLE_SITL_MODE
-uint8_t init_aigle(int port_to_gazebo){
-	aigle_io = IoAigleInterface(port_to_gazebo);
-#else
-uint8_t init_aigle(){
-	aigle_io =  IoAigleInterface();
-#endif
-	if (aigle_io.succeed_init != 1)
-		return aigle_io.succeed_init;
-	// Initialize the strategy to implement
-	strategy = Strategy();
-	return 1;
-}
+int main(void ){
 
-void execute_strategy(){
+	// Initialize aigle io interface with default port and queues elements
+	IoAigleInterface aigle_io;
+	// Initialize strategy class with aigle_io instance and queues elements
+	Strategy strategy(&aigle_io);
+	// Initialize estimator class 
 
-	// try to update sensors values
-	aigle_io.readMotors();
-	strategy.compute_strategies(NULL); // TODO
-
-	MODE situation = strategy.check_situation(NULL); //TO MODIFY ACCORDING TO current GPS data
-	
-	if (situation == MODE_SAFE && aigle_io.new_data){
-		aigle_io.writeMotors(aigle_io.get_motor_data());
-	}/*else {
-		aigle_io.writeMotors(strategy.get_strategy_motors_data());
-	}*/
-}
-
-/*int main(){
-
-	if( init_aigle(14570) != 1){
-		std::cout << "Failed to initialize AIGLE !!" << std::endl;
+	/* Tasks creation */
+	BaseType_t status;
+	/* Create periodic imu read task */
+	status = xTaskCreate(task_imu_read, "imu", STACK_IMU_SIZE, (void *) &aigle_io, SENSOR_READ_PRIORITY, NULL);
+	if (status != pdPASS){
+		printf("[IMU] COULD_NOT_ALLOCATE_REQUIRED_MEMORY \n");
 		return 0;
 	}
-	while(1){
-		execute_strategy();
+
+	/* Create periodic gps read task */
+	status = xTaskCreate(task_gps_read, "gps", STACK_GPS_SIZE, (void *) &aigle_io, SENSOR_READ_PRIORITY, NULL);
+	if (status != pdPASS){
+		printf("[GPS] COULD_NOT_ALLOCATE_REQUIRED_MEMORY \n");
+		return 0;
 	}
+
+	/* Create strategy control task */
+	status = xTaskCreate(task_strategy_control, "strat", STACK_STRAT_SIZE, (void *) &strategy, STRATEGY_PRIORITY, NULL);
+	if (status != pdPASS){
+		printf("[STRAT] COULD_NOT_ALLOCATE_REQUIRED_MEMORY \n");
+		return 0;
+	}
+
+	/* Create EKF estimator task */
+
+
+	#ifdef AIGLE_SITL_MODE
+	/* Create periodic gps read task */
+	status = xTaskCreate(task_collectSensorsData, "sensor", STACK_IMU_SIZE, (void *) &aigle_io, SENSOR_READ_PRIORITY, NULL);
+	if (status != pdPASS){
+		printf("[SENSOR] COULD_NOT_ALLOCATE_REQUIRED_MEMORY \n");
+		return 0;
+	}
+	#endif
+
+	/* Start the scheduler itself. */
+	vTaskStartScheduler();
+
 	return 0;
-}*/
+}
+
+void task_imu_read(void*  aigle_instance){
+	IoAigleInterface *aigle_io = (IoAigleInterface *) aigle_instance;
+	TickType_t nextWakeTime;
+	const TickType_t periodToTick = pdMS_TO_TICKS(IMU_DEADLINE);
+	printf("%s\n", "[Aigle] IMU reading task started");
+	nextWakeTime = xTaskGetTickCount();
+	while (1){
+		vTaskDelayUntil(&nextWakeTime, periodToTick);
+		aigle_io->readIMU();
+	}
+}
+
+void task_gps_read(void*  aigle_instance){
+	IoAigleInterface *aigle_io = (IoAigleInterface *) aigle_instance;
+	TickType_t nextWakeTime;
+	const TickType_t periodToTick = pdMS_TO_TICKS(GPS_DEADLINE);
+	printf("%s\n", "[Aigle] GPS reading task started");
+	nextWakeTime = xTaskGetTickCount();
+	while (1){
+		vTaskDelayUntil(&nextWakeTime, periodToTick);
+		aigle_io->readGPS();
+	}
+}
+
+void task_strategy_control(void* strategy_instance){
+	Strategy *strategy = (Strategy *) strategy_instance;
+	while(1){
+		strategy->execute_strategy();
+	}
+}
+
+void task_ekf_estimator(void* estimator){
+	while(1);
+}
+
+#ifdef AIGLE_SITL_MODE
+void task_collectSensorsData(void* aigle_instance){
+	IoAigleInterface *aigle_io = (IoAigleInterface *) aigle_instance;
+	TickType_t nextWakeTime;
+	const TickType_t periodToTick = pdMS_TO_TICKS(SENSORS_DEADLINE);
+	printf("%s\n", "[Aigle] Sensors reading task started");
+	nextWakeTime = xTaskGetTickCount();
+	while (1){
+		vTaskDelayUntil(&nextWakeTime, periodToTick);
+		aigle_io->collectSensorsData();
+	}
+}
+#endif
+
+/*
+* FreeRTOS that must be implemented in order to compile
+*/
+void vAssertCalled( unsigned long ulLine, const char * const pcFileName )
+{
+    printf("ASSERT: %s : %d\n", pcFileName, (int)ulLine);
+    while(1);
+}
+
+
+unsigned long ulGetRunTimeCounterValue(void)
+{
+    return 0;
+}
+
+void vConfigureTimerForRunTimeStats(void)
+{
+    return;
+}
+
+
+void vApplicationMallocFailedHook(void)
+{
+	while(1);
+}
